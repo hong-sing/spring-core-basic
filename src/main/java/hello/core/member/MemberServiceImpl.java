f3e5387ca65f0c6fffa4f55e413a576e0a5b7cb7
KHS <ewoking777@gmail.com>
2023-06-15 23:13:19 +0900

    객체 지향 원리 적용 - 관심사의 분리

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -2,9 +2,11 @@ package hello.core.member;
 
 public class MemberServiceImpl implements MemberService {
 
-    //여기서 문제는 OCP와 DIP가 지켜지지 않는다는 것이다.
-    //MemberServiceImpl은 MemberRepository에도 의존하고 구현체인 MemoryMemberRepository에도 의존한다.
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final MemberRepository memberRepository;
+
+    public MemberServiceImpl(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
 
     @Override
     public void join(Member member) {
